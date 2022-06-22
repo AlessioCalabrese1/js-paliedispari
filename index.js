fea772b39ba8787d558041d7a90c43c4356ded5d
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-06-22 16:52:33 +0200

    Even and Odd

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+/* PALINDROMA */
+
+
 let userWord = prompt("Inserire una parola!");
 
 palindromeControl(userWord);
@@ -16,3 +20,63 @@ function palindromeControl(userW) {
         console.log("La parola è palindroma!");
     }
 }
+
+
+/* PALINDROMA CON METODI */
+
+/*  let userWord = prompt("Inserire una parola!");
+
+ let userWordReverse = userWord.split("");
+ userWordReverse = userWordReverse.reverse("");
+ userWordReverse = userWordReverse.join("");
+
+ if (userWord === userWordReverse) {
+    console.log("La parola è palindroma!");
+ } else{
+    console.log("La parola NON è palindroma!");
+ } */
+
+
+
+
+
+/* PARI E DISPARI */
+/* let max = 5;
+let min = 1;
+
+let userChoice = prompt("Scegliere pari o dispari!");
+
+while ((userChoice != "pari") && (userChoice != "dispari")) {
+    userChoice = prompt("La scelta inserita NON è corretta, inserire pari o dispari!");
+}
+
+
+let userNumber = parseInt(prompt("Inserire un numero da 1 a 5!"));
+
+while (isNaN(userNumber) || (userNumber < 1) || (userNumber > 5)) {
+    userNumber = parseInt(prompt("Il numero inserito NON è corretto, reinserire il numero!"));
+}
+
+let pcNumber = randomGeneratorNumber(max, min);
+console.log(userNumber);
+let sum = pcNumber + userNumber;
+console.log(sum);
+
+evenOdd(sum); 
+
+
+
+function randomGeneratorNumber(max, min) {
+    return Math.floor((Math.random() * max) + min);
+}
+
+
+function evenOdd(sum) {
+    if ((((sum % 2) === 0) && userChoice === "pari") || (((sum % 2) === 1) && userChoice === "dispari")) {
+        console.log("Ha vinto l'utente!");
+        return;
+    } else{
+        console.log("Ha vinto il pc!");
+        return;
+    }
+} */
